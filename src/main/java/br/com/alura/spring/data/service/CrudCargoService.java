df1de5f6fe1291ec19374bcdef0be8ae7a3f46e1
Juliocbs04 <profile.julio@gmail.com>
2022-01-25 19:26:02 -0300

    Criando a função de atualizar, e criando uma opção de menu para ela onde ela passa o id para atualizar o registro

--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -9,11 +9,31 @@ import java.util.Scanner;
 
 @Service
 public class CrudCargoService {
+    private Boolean system = true;
     @Autowired
     private CargoRepository cargoRepository;
 
     public void inicial(Scanner scanner){
-        salvar(scanner);
+        while(system){
+            System.out.println("Qual ação de cargo deseja executar");
+            System.out.println("0 - Sair");
+            System.out.println("1-  Salvar");
+            System.out.println("2- Atualizar");
+
+            int action = scanner.nextInt();
+            switch(action){
+                case 1:
+                    salvar(scanner);
+                    break;
+                case 2:
+                    atualizar(scanner);
+                    break;
+                default:
+                    system = false;
+                    break;
+            }
+
+        }
     }
 
     private void salvar(Scanner scanner){
@@ -23,6 +43,19 @@ public class CrudCargoService {
         c.setDescricao(descricao);
         this.cargoRepository.save(c);
         System.out.println("Salvo com sucesso! ");
+    }
+
+    private void atualizar(Scanner scanner){
+        System.out.println("Informe o ID: ");
+        int id = scanner.nextInt();
+        System.out.println("Informe a nova Descrição: ");
+        String descricao = scanner.next();
+        Cargo c = new Cargo();
+        c.setId(id);
+        c.setDescricao(descricao);
+
+        this.cargoRepository.save(c);
+        System.out.println("Alterado com sucesso! ");
     }
 
 }
